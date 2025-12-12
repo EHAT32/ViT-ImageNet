4add4ac8256b92fb57f0353e0aa673e956b21035
Roman Kozin <enatik14@gmail.com>
2025-12-12 15:22:29 +0300

    fix typo

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ config = {
     #dropout probs
     'embedding_dropout' : 0.1,
     'ffn_dropoput' : 0.1,
-    'multihead_attention_dropoput' : 0.1,
+    'multihead_attention_dropout' : 0.1,
     'attention_dropout' : 0.1,
     
     #optimizer
